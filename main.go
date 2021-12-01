e3bec2cb44f130cab6dd03aee408de43eebb2975
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-12-01 12:33:32 +0100

    run no ssl for tests

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -10,8 +12,12 @@ import (
 )
 
 var PipeReader *io.PipeReader
+var waitingForID = false
+var unsecure *bool
 
 func main() {
+	unsecure = flag.Bool("u", false, "unsecure")
+	flag.Parse()
 	var err error
 	var w *io.PipeWriter
 	PipeReader, w = io.Pipe()
@@ -22,26 +28,35 @@ func main() {
 			log.Fatal(err)
 		}
 		port.SetRaw()
-		// fmt.Println("listening...")
 		buf := make([]byte, 16)
 		for {
 			_, err := port.Read(buf)
 			if err != nil {
-				log.Fatal(err)
+				fmt.Printf("error while reading from RFID module: %s\n", err)
 			}
-			// fmt.Println(buf)
-			rrr := strings.NewReader(string(buf))
-			io.Copy(w, rrr)
-			w.Close()
-			PipeReader, w = io.Pipe()
+			fmt.Printf("got '%s' from RFID module\n", string(buf[:14]))
+			if waitingForID {
+				buf[14] = 10
+				rrr := strings.NewReader(string(buf[:15]))
+				io.Copy(w, rrr)
+				w.Close()
+				PipeReader, w = io.Pipe()
+			}
 		}
 	}()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		waitingForID = true
 		_, err := io.Copy(w, PipeReader)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("error while sending response: %s\n", err)
 		}
+		fmt.Println("sent as response")
+		waitingForID = false
 	})
-	// fmt.Println("server started on port 8080")
+	if *unsecure {
+		fmt.Println("http server started on port 8040")
+		log.Fatal(http.ListenAndServe(":8040", nil))
+	}
+	fmt.Println("https server started on port 8040")
 	log.Fatal(http.ListenAndServeTLS(":8040", "localhost.crt", "localhost.key", nil))
 }
