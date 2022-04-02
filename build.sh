1bcf342997ae89c820d6a7becaf7581137dffdc9
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-04-02 14:42:06 +0200

    .

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 (git pull && CGO_ENABLED=0 go build -o rfidserver && echo -e "\n${GREEN}build successfull${NC}\n") || echo -e "\n${RED}build failed${NC}\n"
+export RFID_VERSION=$(cat version)
 docker-compose build
