b4de704d1ad1c93ab50030583461638e9e927e42
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-04-02 15:10:18 +0200

    .

--- push.sh
+++ push.sh
@@ -5,5 +5,5 @@ NC='\033[0m' # No Color
 version=$(cat version)
 git pull
 echo "building version ${version}"
-if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; exit 1; fi
-(./build.sh && docker push www.greisslomat.at:5000/rfid:${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
+# if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; exit 1; fi
+(./build.sh && docker push www.greisslomat.at:5000/rfid:$(arch)_${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
