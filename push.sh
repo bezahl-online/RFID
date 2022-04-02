55f3aaa965333b0666570fdd26e7bb1acacaf6b2
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-04-02 15:12:28 +0200

    .

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 version=$(cat version)
 git pull
-echo "building version ${version}"
+echo "building version $(arch)_${version}"
 # if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; exit 1; fi
 (./build.sh && docker push www.greisslomat.at:5000/rfid:$(arch)_${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
