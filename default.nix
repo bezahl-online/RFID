5172c58e2b697eab3a9ee84baa7eac3e8e4a4e56
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2023-07-19 20:07:56 +0200

    nix build

--- default.nix
+++ default.nix
@@ -1,68 +1,41 @@
 { lib
 , buildGoModule
-, fetchFromGitHub
 , nixosTests
 , testers
 , installShellFiles
 }:
 let
   version = "1.0.1";
-  dist = fetchFromGitHub {
-    owner = "bezahl-online";
-    repo = "RFID";
-    rev = "v${version}";
-    hash = "sha256-SJO1q4g9uyyky9ZYSiqXJgNIvyxT5RjrpYd20YDx8ec=";
-  };
+  owner = "bezahl-online";
+  repo = "RFID";
+  rev = "v${version}";
+  sha256 = "1ikyp1rxrl8lyfbll501f13yir1axighnr8x3ji3qzwin6i3w497";
 in
 buildGoModule {
   pname = "rfidserver";
   inherit version;
 
-  src = fetchFromGitHub {
-    owner = "kassa";
-    repo = "bezahl-online";
-    rev = "v${version}";
-    hash = "sha256-3a3+nFHmGONvL/TyQRqgJtrSDIn0zdGy9YwhZP17mU0=";
-  };
+  src = ./.;
+ 
+  vendorSha256 = "sha256-GNZhzOp4orMBgJXIozOsswZXn7QR/ji1NFYwLFiw/3c=";
 
-  vendorHash = "sha256-toi6efYZobjDV3YPT9seE/WZAzNaxgb1ioVG4txcuXM=";
-
-  subPackages = [ "cmd/caddy" ];
-
-  ldflags = [
-    "-s" "-w"
-    "-X github.com/caddyserver/caddy/v2.CustomVersion=${version}"
-  ];
-
-  nativeBuildInputs = [ installShellFiles ];
-
-  postInstall = ''
-    install -Dm644 ${dist}/init/caddy.service ${dist}/init/caddy-api.service -t $out/lib/systemd/system
-
-    substituteInPlace $out/lib/systemd/system/caddy.service --replace "/usr/bin/caddy" "$out/bin/caddy"
-    substituteInPlace $out/lib/systemd/system/caddy-api.service --replace "/usr/bin/caddy" "$out/bin/caddy"
-
-    $out/bin/caddy manpage --directory manpages
-    installManPage manpages/*
-
-    installShellCompletion --cmd caddy \
-      --bash <($out/bin/caddy completion bash) \
-      --fish <($out/bin/caddy completion fish) \
-      --zsh <($out/bin/caddy completion zsh)
+  buildPhase = ''
+    runHook preBuild
+    CGO_ENABLED=0 go build -o rfidserver .
+    runHook postBuild
   '';
 
-  passthru.tests = {
-    inherit (nixosTests) caddy;
-    version = testers.testVersion {
-      command = "${caddy}/bin/caddy version";
-      package = caddy;
-    };
-  };
+  installPhase = ''
+    mkdir -p $out/bin
+    mv rfidserver $out/bin
+    cp localhost.crt localhost.key $out/bin
+  '';
 
   meta = with lib; {
-    homepage = "https://caddyserver.com";
-    description = "Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ Br1ght0ne emilylange techknowlogick ];
+    homepage = "https://github.com/bezahl-online/RFID";
+    description = "RFID server code";
+    license = licenses.mit;
+    maintainers = with maintainers; [ /* list of maintainers here */ ];
   };
 }
+
